9b54a8d643666ce07116e704e8043d17141f9371
markeloff <fnatic_forest@yeah.net>
2014-07-18 10:32:05 +0800

    删除不需要的包

--- src/main/java/com/suning/controller/PrePassPortController.java
+++ src/main/java/com/suning/controller/PrePassPortController.java
@@ -1,8 +1,5 @@
 package com.suning.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
