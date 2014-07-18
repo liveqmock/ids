f34b937672c3dde8c66cb48e361f32ddcb9a052e
markeloff <fnatic_forest@yeah.net>
2014-07-18 11:36:55 +0800

    删除无效的包

--- src/main/java/com/suning/service/impl/LoginInfoHandleServiceImpl.java
+++ src/main/java/com/suning/service/impl/LoginInfoHandleServiceImpl.java
@@ -2,8 +2,6 @@ package com.suning.service.impl;
 
 import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.suning.constant.IdsConstant;
